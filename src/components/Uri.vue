f846435c3f546e613223ea0f8fe2bf1f9b2945a6
Olivier Filangi <olivier.filangi@inrae.fr>
2022-05-23 14:56:55 +0200

    import service

--- src/components/Uri.vue
+++ src/components/Uri.vue
@@ -1,18 +1,23 @@
-<script setup>
-import { SWDiscoveryConfiguration, SWDiscovery, URI, SWTransaction} from '@p2m2/discovery'
+<script lang="ts" setup>
+
+import { DiscoveryService } from './discovery.service'
+
+
 console.log('hello script setup')
 const msg = "Something"
+const config = DiscoveryService
+/*
 const config = SWDiscoveryConfiguration
                  .init()
-                 .sparqlEndpoint("https://forum.semantic-metabolomics.fr/sparql/")
+              //   .sparqlEndpoint("https://forum.semantic-metabolomics.fr/sparql/")
                  .setPageSize(5)
                  .setSizeBatchProcessing(10)
                  .setLogLevel("debug")
                  .setCache(false);
 
-const disco = SWDiscovery(config).something("h1").select("h1").commit().raw();
+const disco = SWDiscovery(config).something("h1").select("h1").commit().raw();*/
 </script>
 
 <template>
-  <div>{{ disco }}</div>
+  <div>{{ config.toString() }}</div>
 </template>
