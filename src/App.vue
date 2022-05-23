60bfb66aee946bd02d579ce856ff20f0f411b856
Olivier Filangi <olivier.filangi@inrae.fr>
2022-05-23 13:21:56 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <v-toolbar app dark> 
+      MetaboHUB - Resource Description
+    </v-toolbar> 
+      <v-main>
+        <Uri/>
+      </v-main>
+  </v-app>
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+import Uri from './components/Uri.vue'
+
+export default {
+  name: 'App',
+
+  components: {
+    Uri,
+  },
+
+  data: () => ({
+    //
+  }),
+}
+</script>
---
+++ src/components/Uri.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { SWDiscoveryConfiguration, SWDiscovery, URI, SWTransaction} from '@p2m2/discovery'
+console.log('hello script setup')
+const msg = "Something"
+const config = SWDiscoveryConfiguration
+                 .init()
+                 .sparqlEndpoint("https://forum.semantic-metabolomics.fr/sparql/")
+                 .setPageSize(5)
+                 .setSizeBatchProcessing(10)
+                 .setLogLevel("debug")
+                 .setCache(false);
+
+const disco = SWDiscovery(config).something("h1").select("h1").commit().raw();
+</script>
+
+<template>
+  <div>{{ disco }}</div>
+</template>
